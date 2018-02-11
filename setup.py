248cd250fca041b4a7d306e4dae4be01700b0e3c
mdolgun <m.dolgun@yahoo.com>
2018-02-11 20:37:34 +0300

    init

--- setup.py
+++ setup.py
@@ -6,6 +6,12 @@ setup(name='GLRParser',
       url='https://github.com/mdolgun/GLRParser',
       author='Mehmet Dolgun',
       author_email='m.dolgun@yahoo.com',
+	  classifiers=[
+	      'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+		  'Programming Language :: Python :: 3.6'
+	  ],
+	  keywords = 'NLP MachineTranslation Parser GLR Turkish',
       license='MIT',
       packages=['GLRParser']
 )
