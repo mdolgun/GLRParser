18abda73995d5181d826e4c75cf831bd3487960b
mdolgun <m.dolgun@yahoo.com>
2018-02-11 17:58:15 +0300

    Initial

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='GLRParser',
+      version='0.1.0',
+      description='A GLR Parser for Natural Language Processing and Translation',
+      url='https://github.com/mdolgun/GLRParser',
+      author='Mehmet Dolgun',
+      author_email='m.dolgun@yahoo.com',
+      license='MIT',
+      packages=['GLRParser']
+)
