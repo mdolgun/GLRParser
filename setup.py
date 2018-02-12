05caffa10e1306df72dc5662353bdba0cd987c9c
mdolgun <m.dolgun@yahoo.com>
2018-02-12 14:10:30 +0300

    init

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
+import os
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname),encoding='utf-8').read()
+	
 setup(name='GLRParser',
-	version='0.1.2',
+	version='0.1.4',
 	description='A GLR Parser for Natural Language Processing and Translation',
+	long_description=read("README.rst"),
 	url='https://github.com/mdolgun/GLRParser',
 	author='Mehmet Dolgun',
 	author_email='m.dolgun@yahoo.com',
