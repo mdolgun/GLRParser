fe4cbff555cc20f2b67fcfd1a4056e0126b6b8c6
mdolgun <m.dolgun@yahoo.com>
2018-02-25 15:15:43 +0300

    init

---
+++ GLRParser/grammar.py
@@ -0,0 +1,272 @@
+""" Recursive Descent Grammar Parser 
+
+(c) 2018 by Mehmet Dolgun, m.dolgun@yahoo.com
+
+Grammar Rules:
+Rule ::= Head "->" Prod [ ":" Prod ] [ Feat ] [ "#" Comment ]
+Prod ::= ( Terminal | NonTerminal [ "-" Suffix ] [ FParam ] )*
+Feat ::= "[" [ Name "=" Value ( "," Name "=" Value )* ] "]"
+FParam ::= "(" [ Name [ "=" Value ] ( "," Name [ "=" Value ] )* ")"
+where 
+Terminal ::= Id starting with upper case, excluding "-"
+NonTerminal ::= Id not starting with upper case, optionally double-quoted
+Name, Value ::= Id
+
+"""
+import re
+from collections import namedtuple
+
+class GrammarError(Exception):
+    pass
+
+empty_list = list()
+empty_dict = dict()
+
+Rule = namedtuple('Rule', 'head, left, right, feat, lparam, rparam, lcost, rcost')
+#print(Rule._source)
+
+def format_feat(fdict,par):
+    if not fdict:
+        if type(fdict)==dict: # empty dict
+            return par
+        return ""
+    return par[0] + ",".join(["=".join(item) if item[1] else item[0] for item in fdict.items()]) + par[1]
+
+def format_rule(self):
+    return "%s -> %s : %s %s" % (
+        self.head, 
+        " ".join( ['"'+symbol+'"' if fparam is False else symbol+format_feat(fparam,'()') for symbol,fparam in zip(self.left,self.lparam)] ),
+        " ".join( ['"'+symbol+'"' if fparam is False else str(symbol)+format_feat(fparam,'()') for symbol,fparam in zip(self.right,self.rparam)] ),
+        format_feat(self.feat,'[]')
+    )
+
+Rule.format = format_rule
+
+class Grammar:
+    NONTERM = re.compile('[_A-Z][-_A-Za-z0-9]*')
+    TERM = re.compile('".*"|[-a-zıüöçğşðþý][-\'A-Za-zıüöçğşðþý]*')
+    FEAT = re.compile('\*?[a-z0-9_]*')
+    def add_range(chars,start,end):
+        for ch in range(ord(start),ord(end)+1):
+            chars.add(chr(ch))
+    word_chars = set('_-ıüöçğşðþý"\'') 
+    add_range(word_chars,'A','Z')
+    add_range(word_chars,'a','z')
+    add_range(word_chars,'0','9')
+   
+    def __init__(self,s,line=0):
+        self.s = s
+        self.pos = 0
+        self.line = line
+
+    def get_rest(self,maxchars=20):
+        return self.s[self.pos:self.pos+maxchars]
+
+    def get_eof(self,ensure=True):
+        self.skip_ws()
+        if self.pos == len(self.s) or self.s[self.pos] == '#':
+            return True
+        if ensure:
+            raise GrammarError("Line:%d Pos:%d EOF expected but found: %s..." % (self.line,self.pos,self.get_rest()))
+
+    def get_token(self,token,ensure=True,skip_ws=True):
+        if skip_ws:
+            self.skip_ws()
+        if self.s.startswith(token,self.pos):
+            self.pos += len(token)
+            return True
+        if ensure:
+            raise GrammarError("Line:%d Pos:%d '%s' expected but found: %s..." % (self.line,self.pos,token,self.get_rest()))
+
+    def skip_ws(self):
+        try:
+            while self.s[self.pos] in " \t\r\n":
+                self.pos += 1 
+        except IndexError:
+            pass
+
+    def get_word(self,ensure=True,skip_ws=True):
+        if skip_ws:
+            self.skip_ws()
+        start = self.pos
+        try:
+            while self.s[self.pos] in Grammar.word_chars:
+                self.pos += 1 
+        except IndexError:
+            pass
+        if start != self.pos:
+            return self.s[start:self.pos]
+        if ensure:
+            raise GrammarError("Line:%d Pos:%d Word expected but found: %s..." % (self.line,self.pos,self.get_rest()))
+
+    def get_symbol(self,ensure=True,skip_ws=True):
+        word = self.get_word(ensure,skip_ws)
+        if word:
+            if Grammar.NONTERM.fullmatch(word):
+                return word,0
+            if Grammar.TERM.fullmatch(word):
+                return word.strip('"'),1
+            if ensure:
+                raise GrammarError("Line:%d Pos:%d Symbol expected but found: '%s'" % (self.line,self.pos,word))
+        return None,None
+
+    def get_nonterm(self,ensure=True,skip_ws=True):
+        word = self.get_word(ensure,skip_ws)
+        if word:
+            if Grammar.NONTERM.fullmatch(word):
+                return word
+            if ensure:
+                raise GrammarError("Line:%d Pos:%d NonTerm expected but found: '%s'" % (self.line,self.pos,word))
+
+    def get_feat(self,ensure=True,skip_ws=True):
+        word = self.get_word(ensure,skip_ws)
+        if word:
+            if Grammar.FEAT.fullmatch(word):
+                return word
+            if ensure:
+                raise GrammarError("Line:%d Pos:%d FeatId expected but found '%s'" % (self.line,self.pos,word))
+
+    def parse_rule(self,reverse=False):
+        """ parses a rule and return an object of type Rule """
+        if self.get_eof(False):
+            return None
+
+        head = self.get_nonterm()
+
+        self.get_token('->')
+
+        left,lparam = self.parse_prod()
+        if self.get_token(':',False):
+            right,rparam = self.parse_prod()
+        else:
+            right,rparam = empty_list,empty_list
+        if self.get_token('[',False):
+            feat = self.parse_feat()
+        else:
+            feat = empty_dict
+        self.get_eof()
+
+        lcost = 0
+        rcost = 0
+
+        if reverse:
+            left,lparam,lcost,right,rparam,rcost = right,rparam,rcost,left,lparam,lcost
+        
+        # following cross-references right with left, removing referencing suffixes
+        #  e.g.  VP -> give NP-prim NP-secn : NP-prim -yA NP-secn ver => VP -> give NP NP : 1 -yA 2 ver
+        for idx,(symbol,param) in enumerate(zip(right,rparam)):
+            if param is not False: # NonTerminal
+                try:
+                    right[idx] = right.index(symbol)
+                except ValueError:
+                    right[idx] = symbol.split('-')[0]
+
+        for idx,(symbol,param) in enumerate(zip(left,lparam)):
+            if param is not False: # NonTerminal
+                left[idx] = symbol.split('-')[0]    
+
+        return Rule(head,left,right,feat,lparam,rparam,lcost,rcost)
+
+    def parse_prod(self):
+        """ parses left or right grammar and builds references, returns tuple(symbol list,param list)  """
+        prod = []
+        param_list = []
+        symbol,stype = self.get_symbol(False)
+        while symbol:
+            if stype == 0: # NonTerminal
+                if self.get_token('(', False, skip_ws=False):
+                    param = self.parse_fparam()
+                else:
+                    param = None
+            else:
+                param = False   
+            prod.append(symbol)
+            param_list.append(param)
+            symbol,stype = self.get_symbol(False)
+        return prod,param_list
+
+    def parse_feat(self):
+        """ Parses feature list returns dict of name=value """
+        name = self.get_feat(False)
+        if not name and self.get_token(']',False): # empty list
+            return empty_dict
+        fdict = dict()
+        self.get_token('=')
+        value = self.get_feat()  
+        fdict[name] = value
+
+        while self.get_token(',', False):   
+            name = self.get_feat()
+            self.get_token('=')
+            value = self.get_feat()  
+            fdict[name] = value
+        self.get_token(']')
+        return fdict
+
+    def parse_fparam(self):
+        """ Parses feature parameter list returns dict of name=value or name=None """
+        name = self.get_feat(False)
+        if not name and self.get_token(')',False): # empty list
+            return empty_dict
+        fdict = dict()
+        if self.get_token('=', False):
+            value = self.get_feat()
+            fdict[name] = value
+        else:
+            fdict[name] = None
+        while self.get_token(',', False):   
+            name = self.get_feat()
+            if self.get_token('=', False):
+                value = self.get_feat()
+                fdict[name] = value
+            else:
+                fdict[name] = None
+        self.get_token(')')
+        return fdict
+
+    def load_grammar(fname,reverse=False):
+        """ loads a grammar file and parse it """
+        rules = []
+        rules.append(Grammar("_S_ -> S : S").parse_rule())
+        with open(fname, "r") as f:
+            line = 0
+            for s in  f:
+                line += 1
+                rule = Grammar(s,line).parse_rule()
+                if rule is None: # empty/comment line
+                    continue
+                rules.append(rule)
+        return rules
+
+
+
+def main():
+    text = """
+    VP -> Modal Ven-1 "isn't" -'s  : Ven-1(mode=pres,pers,numb) Tense() -iyor [mode=pres_perf] # a comment
+    VP -> [pers=1] @ test
+    VP -> : çalışıyor [pers=1]
+    VP ->
+    """
+    line = 0
+    for s in text.split('\n'):
+        line += 1
+        g = Grammar(s,line)
+        print("Parsing: %s" % s)
+        try:
+            rule = g.parse_rule()
+            if rule is None:
+                continue
+            #print(rule)
+            print('    ',rule.format())
+        except GrammarError as pe:
+            print('    ',pe.args[0])
+
+def fmain():
+    rules = Grammar.load_grammar("tenses.grm")
+    for rule in rules:
+        print(rule.format())
+if __name__ == "__main__":
+    # execute only if run as a script
+    fmain()
+
+      
---
+++ GLRParser/morpher.py
@@ -0,0 +1,130 @@
+""" Morphological Post-Processing for Turkish
+
+(c) 2018 by Mehmet Dolgun, m.dolgun@yahoo.com
+
+All functionality is provided by main class: TurkishPostProcessor
+
+    special characters
+    Vowel Harmony:
+    ^ : make previous vowel back, i.e. aIuo->eiUO e.g kol-A -> kola, ro^l-A -> role
+    H : high vowels, using harmony rules aI->I, ei->i, uo->u, UO->U  e.g ev-Hm -> evim, yol-Hm -> yolum
+    A : low,unrounded vowels, using harmony rules aIuo->a, eiUO->e e.g. ev-lAr -> evler yol-lAr -> yollar
+    Consonants
+    N : used as last char of same suffixes (e.g. 3d sing possesive), realized to n only if it is followed by a vowel e.g. tereyağıN-ı->tereyağını, tereyağıN-ım ->tereyağım
+    $ : drops the next vowel if it takes a vowel-starting suffix e.g. bur$un-u -> burnu, bur$un-da -> burunda
+    + : duplicates previous letter if it takes a vowel-starting suffix e.g. hak+-ı -> hakkı, hak+-ta -> hakta
+    ! : prevents softening of previous consonant e.g. kitap-ı -> kitabı, hap!-ı -> hapı
+	
+	Note that ^, $, +, !, N symbols is used only in word root, N is required for compound nouns
+
+"""
+
+import re
+
+class TurkishPostProcessor:
+    """
+    rules = [
+        ( "(?<=[aIeiouOU]-)(H)" , "" ),
+        ( "(?<=[^aIeiouOU]-)([nsy])", "" ),
+        ( "(N-y)(?=[aIeiouOUHA])", "n" ),
+        ( "(N-y)(?=[^aIeiouOUHA])", "y" ),
+        ( "([pCtk])-(?=[snHA]|y[aIeiouOUHA])", {'p':'b', 'C':'c', 't':'d', 'k':'G'} ),
+        ( "([pbctdk]\+)-(?=[snHA]|y[aIeiouOUHA])", {'p+':'bb', 'b+':'bb', 'c+':'cc', 't+':'tt', 'd+':'dd', 'k+':'kk' } ),
+        ( "(\$[IiuU])(?=[^aIeiouOU]-(?:[snHA]|y[aIeiouOUHA]))", ""),
+        ( "(enk)-(?=[snHA]|y[aIeiouOUHA])", "eng"),
+        ( "(N)-", "n"),
+        ( "(?<=[pCtkSfsh]-)(d)","t"),
+        ( "(?<=[pCtkSfsh][\+!]-)(d)","t"),
+        ( "([-\$\+!]|N$)", "")
+    ]
+    """
+    rules = [
+        ( "(?<=[aIeiouOU]-)(H)" , "" ),
+        ( "(?<=[^aIeiouOU]-)([NZY])", "" ),
+        ( "(?<=[aIeiouOU]-)([NZY])", {'N':'n', 'Z':'s', 'Y':'y'} ),
+        ( "(N-Y)(?=[aIeiouOUHA])", "n" ),
+        ( "(N-Y)(?=[^aIeiouOUHA])", "y" ),
+        ( "([pCtk])-(?=[ZNHA]|Y[aIeiouOUHA])", {'p':'b', 'C':'c', 't':'d', 'k':'G'} ),
+        ( "([pbctdk]\+)-(?=[ZNHA]|Y[aIeiouOUHA])", {'p+':'bb', 'b+':'bb', 'c+':'cc', 't+':'tt', 'd+':'dd', 'k+':'kk' } ),
+        ( "(\$[IiuU])(?=[^aIeiouOU]-(?:[ZNYHA]|Y[aIeiouOUHA]))", ""),
+        ( "(enk)-(?=[ZNHA]|Y[aIeiouOUHA])", "eng"),
+        ( "(N)-", "n"),
+        ( "(?<=[pCtkSfsh]-)(d)","t"),
+        ( "(?<=[pCtkSfsh][\+!]-)(d)","t"),
+        ( "([-\$\+!]|N$)", "")
+    ]
+
+    vowel = { 'a': 0, 'I':0 , 'e':1, 'i':1, 'o':2, 'u':2 , 'O':3, 'U':3 }
+    H = [ 'I', 'i', 'u', 'U' ]
+    A = [ 'a', 'e', 'a', 'e' ]
+
+    outtab = { 71:'ğ', 85:'ü', 79:'ö', 67:'ç', 83:'ş', 73:'ı' } # internal to unicode
+    #outtab = { 71:'ð', 85:'ü', 79:'ö', 67:'ç', 83:'þ', 73:'ý' } # internal to codepage 
+    intab  = { # to internal
+        252:'U', 220:'U', 246:'O', 214:'O', 231:'C', 199:'C', # from common unicode & codepage
+        240:'G', 208:'G', 222:'S', 222:'S', 253:'I', 221:'i', # from code page 
+        287:'G', 286:'G', 351:'S', 350:'S', 305:'I', 304:'i'  # from unicode 
+    }    
+    def __init__(self):
+        relist, self.replist = zip(*TurkishPostProcessor.rules)
+        self.rx = re.compile('|'.join(relist))
+
+        
+    def handle_match(self,match):
+        for idx,val in enumerate(match.groups()):
+            if val:
+                repl = self.replist[idx]
+                if type(repl) == dict:
+                    return repl[val]
+                else:
+                    return repl
+            
+    def vowel_harmony(word):
+        voweltype = 0
+        out = []
+        for char in word:
+            if char in TurkishPostProcessor.vowel:
+                voweltype = TurkishPostProcessor.vowel[char]
+            elif char == 'H':
+                char = TurkishPostProcessor.H[voweltype]
+            elif char == 'A':
+                char = TurkishPostProcessor.A[voweltype]
+                voweltype &= 1
+            elif char == '^':
+                voweltype |= 1
+                continue
+            out.append(char)
+        return "".join(out)
+    
+    
+    def __call__(self,text):
+        return TurkishPostProcessor.vowel_harmony(
+            self.rx.sub(
+                self.handle_match,
+                text.replace(' -','-').translate(TurkishPostProcessor.intab)
+            )
+        ).translate(TurkishPostProcessor.outtab)
+
+def main():
+    morpher = TurkishPostProcessor()
+
+    words = [ "ev", "araba", "kitap", "hak+", "tank!", "yatak", "bur$un", "renk", "ro^l" ]
+    suffixes = [ "-Hm", "-YH", "-YlA", "-dA", "-NHn", "-ZHN", "-ZHN-YH", "-ZHN-YlA", "-ZHN-dA", "-ZHN-NHn" ]
+
+    for word in words:
+        for suffix in suffixes:
+            nword = word + suffix
+            print(nword, morpher(nword))
+
+    words = [ "gel", "git", "oku", "ara" ]
+    suffixes = [ "-dH-m", "-dH-n", "-dH-k", "-dH-nHz", "-Hyor-Hm", "-Hyor-sHn", "-Hyor-Hz", "-Hyor-sHnHz", "-YAcAk-Hm", "-YAcAk-sHn", "-YAcAk-Hz", "-YAcAk-sHnHz" ]
+
+    for word in words:
+        for suffix in suffixes:
+            nword = word + suffix
+            print(nword, morpher(nword))
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+
